822f958d1b32189df03806e5a401572126a85182
Yonashenok <yonashenok24@gmail.com>
2022-03-11 10:33:10 -0800

    task 2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,14 +4,14 @@
 /**
  * main - Entry point
  *
- * return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n;
 	int L;
 	char str[] = "Last digit of";
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	L = n % 10;
