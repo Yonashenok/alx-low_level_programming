7e9065a1c9089d6ac59d9ddecfa9540ed7f043bf
Yonashenok <yonashenok24@gmail.com>
2022-03-15 11:24:48 -0700

    alphabet lower case

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,23 @@
 #include "main.h"
 
+/**
+ * main - prints Holberton
+ *
+ * Description: prints Holberton
+ *
+ * Return: 0 (Success)
+ */
+
 int main(void)
 {
-	int i=0;
-	char taxt[10] = "_putchar";
-	for (i=0; i<8; i++)
+	char text[10] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
 	{
-		_putchar(taxt[i]);
+		_putchar(text[i]);
 	}
 	_putchar('\n');
 
-	return(0);
+	return (0);
 }
