a4fed14e8e66e72310b77dddc915fa5652deda68
Yonashenok <yonashenok24@gmail.com>
2022-04-13 10:17:11 -0700

    new commit

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -9,28 +9,28 @@
   */
 int main(int argc, char *argv[])
 {
-  int count, bytes;
+	int count, bytes;
 
-  if (argc != 2)
-  {
-    printf("Error\n");
-    exit(1);
-  }
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
 
-  bytes = atoi(argv[1]);
-  if (bytes < 0)
-  {
-    printf("Error\n");
-    exit(2);
-  }
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
 
-  for (count = 0; count < bytes; count++)
-  {
-    printf("%02hhx", *((char *)main + count));
-    if (count  < bytes - 1)
-      printf(" ");
-    else
-      printf("\n");
-  }
-  return (0);
+	for (count = 0; count < bytes; count++)
+	{
+		printf("%02hhx", *((char *)main + count));
+		if (count  < bytes - 1)
+			printf(" ");
+		else
+			printf("\n");
+	}
+	return (0);
 }
