aea897515a2ea8c8b3a39294d667a6e05791dccf
Yonashenok <yonashenok24@gmail.com>
2022-06-13 10:58:17 -0700

    ctype module

---
+++ 0x18-dynamic_libraries/my_functions.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int add();
+int sub();
+int mul();
+int div();
+int mod();
+
+
+
+int add(int a, int b) 
+{
+	return a + b;
+}
+
+int sub (int a, int b)
+{
+	return a - b;
+}
+
+int mul(int a, int b)
+{
+	return a * b;
+}
+
+int div(int a, int b)
+{
+	return a/b;
+}
+
+int mod(int a, int b)
+{
+	return a % b;
+}
